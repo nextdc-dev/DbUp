f3b627611cb7000e1c370abd7c0be63fcfd854ef
Brendon Atkins <brendon.atkins@nextdc.com>
2022-03-31 08:12:25 +1000

    Add support for Cassandra database
    Pull in DataStax C# driver to interact with Cassandra 3.
    
    Add support for script execution and journal.
    Using "schema" variable to represent a keyspace. Does not create
     keyspace - if keyspace is provided then assumes it is present. DBUp can
     be used to execute a script to setup keyspace if running with a
     NullJournal.

---
+++ src/dbup-cassandra/CassandraScriptExecutor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Cassandra;
+using DbUp.Engine;
+using DbUp.Engine.Output;
+using DbUp.Engine.Transactions;
+using DbUp.Support;
+
+namespace dbup_cassandra
+{
+    public class CassandraScriptExecutor : ScriptExecutor
+    {
+        public CassandraScriptExecutor(Func<IConnectionManager> connectionManagerFactory, Func<IUpgradeLog> log, string keyspace, Func<bool> variablesEnabled,
+            IEnumerable<IScriptPreprocessor> scriptPreprocessors, Func<IJournal> journalFactory) 
+            : base(connectionManagerFactory, new CassandraObjectParser(), log, keyspace, variablesEnabled, scriptPreprocessors, journalFactory)
+        {
+        }
+
+        protected override void ExecuteCommandsWithinExceptionHandler(int index, SqlScript script, Action executeCommand)
+        {
+            try
+            {
+                executeCommand();
+            }
+            catch (DriverException cqlException)
+            {
+                Log().WriteInformation("CQL Driver exception has occured in script: '{0}'", script.Name);
+                Log().WriteError("Script block number: {0}; Message: {1}", index, cqlException.Message);
+                Log().WriteError(cqlException.ToString());
+                throw;
+            }
+        }
+
+        protected override string GetVerifySchemaSql(string keyspace)
+        {
+            return $"USE {keyspace};";
+        }
+    }
+}
---
+++ src/dbup-cassandra/CassandraTableJournal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DbUp.Engine.Output;
+using DbUp.Engine.Transactions;
+using DbUp.Support;
+
+namespace dbup_cassandra
+{
+    public class CassandraTableJournal : TableJournal
+    {
+        /// <inheritdoc/>
+        public CassandraTableJournal(Func<IConnectionManager> connectionManager, Func<IUpgradeLog> logger, string schema, string table) 
+            : base(connectionManager, logger, new CassandraObjectParser(), schema?.ToLower(), table)
+        {
+        }
+
+        protected override string GetInsertJournalEntrySql(string @scriptName, string @applied)
+        {
+            return $"insert into {FqSchemaTableName} (id, script_name, applied) values ({Guid.NewGuid()},:" + scriptName.Replace("@", "") + ",:" + applied.Replace("@", "") + ")";
+        }
+
+        protected override string GetJournalEntriesSql()
+        {
+            return $"select script_name from {FqSchemaTableName}";
+        }
+
+        protected override string CreateSchemaTableSql(string quotedPrimaryKeyName)
+        {
+            return
+$@"create table {FqSchemaTableName} (
+    id UUID PRIMARY KEY,
+    script_name text,
+    applied timestamp
+)";
+        }
+
+        protected override string DoesTableExistSql()
+        {
+            return string.Format("select count(*) from system_schema.tables where keyspace_name='{1}' and table_name = '{0}'", UnquotedSchemaTableName, SchemaTableSchema);
+        }
+    }
+}
